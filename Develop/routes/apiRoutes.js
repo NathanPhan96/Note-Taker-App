4168b99c61acc7e1713e83297c3e25d44816c2a7
Nathan Phan <nathanphan96@yahoo.com>
2022-12-13 19:33:15 -0800

    added code to apiRoutes.js and created help.js to complete app functionality

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const help = require('../Helper/help');
+const fs = require('fs');
 
 router.get('/', function (req, res) {
 
-    res.json(`Received your ${req.method} request}`)
+    console.info(`Received your ${req.method} request}`)
     const dataFromJSON = fs.readFileSync('./db/db.json', 'utf8');
     res.json(JSON.parse(dataFromJSON));
 
@@ -10,14 +12,32 @@ router.get('/', function (req, res) {
 
 router.post('/', function (req, res) {
 
-    res.json(`Received your ${req.method} request`);
+    console.info(`Received your ${req.method} request`);
     const { title, text } = req.body;
+    if (title && text) {
     const newNote = {
         title,
         text,
+        id: help(),
     };
     const currentNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     currentNotes.push(newNote);
+
+    fs.writeFile('./db/db.json', JSON.stringify(currentNotes), (err) =>
+    err
+    ? console.err(err)
+    : console.log(`Note for ${newNote.title} has been taken`)
+    );
+    const response = {
+        status: 'Saved Note',
+        body: newNote,
+    };
+
+    console.log(response)
+    res.status(201).json(response);
+    } else {
+        res.status(500).json('Your note was not Saved');
+    }
 });
 
 
